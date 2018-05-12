ef72f82e6f076466dc6869742630ff956b8ef8ad
mishokgerasimoff <37957001+mishokgerasimoff@users.noreply.github.com>
2018-05-12 17:06:38 +0300

    приглашения в группы

---
+++ vk_bot/123.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace vk_bot
+{
+    partial class _123
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.pictureBoxAvatar = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAvatar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Location = new System.Drawing.Point(81, 33);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(35, 13);
+            this.labelFirstName.TabIndex = 0;
+            this.labelFirstName.Text = "label1";
+            this.labelFirstName.Click += new System.EventHandler(this.labelFirstName_Click);
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(81, 9);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(35, 13);
+            this.labelLastName.TabIndex = 1;
+            this.labelLastName.Text = "label1";
+            this.labelLastName.Click += new System.EventHandler(this.labelLastName_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(147, 1);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(450, 268);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // pictureBoxAvatar
+            // 
+            this.pictureBoxAvatar.Location = new System.Drawing.Point(2, 1);
+            this.pictureBoxAvatar.Name = "pictureBoxAvatar";
+            this.pictureBoxAvatar.Size = new System.Drawing.Size(73, 76);
+            this.pictureBoxAvatar.TabIndex = 3;
+            this.pictureBoxAvatar.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(2, 114);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 174);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(147, 275);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(450, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // _123
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 300);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBoxAvatar);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.labelFirstName);
+            this.Name = "_123";
+            this.Text = "_123";
+            this.Click += new System.EventHandler(this._123_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAvatar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelFirstName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.PictureBox pictureBoxAvatar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ vk_bot/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace vk_bot
+{
+    public partial class Form1 : Form
+    {
+        string access_token;
+        string userId;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Dock = DockStyle.Fill;
+            webBrowser1.Navigate("https://oauth.vk.com/authorize?client_id=6410347&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends+groups&response_type=token&v=5.73");
+       
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            string url = e.Url.ToString();
+            if (url.Contains("error"))
+            {
+                MessageBox.Show("Ошибка");
+            }
+            if (url.Contains("access_token"))
+            {
+                //Выделяю access_token
+                int IndexAccTok = url.IndexOf("access_token");
+                access_token = url.Remove(0, IndexAccTok + 13);
+                int IndexAmp = access_token.IndexOf("&");
+                access_token = access_token.Remove(IndexAmp);
+
+                //Зпрашиваю информацию о пользователе
+                
+                //Создаю XML документ
+                XmlDocument doc = new XmlDocument();
+                doc.Load("https://api.vk.com/method/users.get.xml?fields=photo_100&access_token="+access_token+"&v=5.73");
+                XmlNode response = doc.SelectSingleNode("response");
+                XmlNode user = response.SelectSingleNode("user");
+
+                XmlNode FirstName = user.SelectSingleNode("first_name");
+                labelFirstName.Text = FirstName.InnerText;
+             
+                XmlNode LastName = user.SelectSingleNode("last_name");
+                labelLastName.Text = LastName.InnerText;
+
+                userId = user.SelectSingleNode("userId").InnerText;
+              //XmlNode user_id = ;
+               //labeluser_id.Text = user_id.InnerText;
+
+                pictureBoxAvatar.ImageLocation = user.SelectSingleNode("photo_100").InnerText;
+                webBrowser1.Visible = false;
+            }
+
+        }
+
+        private void buttonWelkom_Click(object sender, EventArgs e)
+        {
+            _123 frm = new _123();
+            frm.Access_token = access_token;
+            frm.userId = userId;
+            frm.Show();
+        }
+    }
+}
