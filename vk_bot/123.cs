d01f15cf6b9b46e51567cd62dd3434c76230e45f
mishokgerasimoff <37957001+mishokgerasimoff@users.noreply.github.com>
2018-06-07 17:46:18 +0300

    приглашения в группы 3

--- vk_bot/123.cs
+++ vk_bot/123.cs
@@ -60,15 +60,20 @@ namespace vk_bot
             {
                 XmlDocument doc = new XmlDocument();
                 doc.Load("https://api.vk.com/method/friends.get.xml?user_id=" + userId + "&access_token=" + Access_token + "&v=5.73");
+
                 XmlNode response = doc.SelectSingleNode("response");
                 XmlNode items = response.SelectSingleNode("items");
                 foreach (XmlNode a in items.SelectNodes("user_id"))
                 {
                     textBox1.Text = textBox1.Text + a.InnerText + " ";
-
+                    XmlDocument doc3 = new XmlDocument();
+                    doc3.Load("https://api.vk.com/method/groups.getById.xml?group_id="+textBox2.Text+"&access_token="+Access_token+"&v=5.52");
+                    XmlNode response2 = doc3.SelectSingleNode("response");
+                    XmlNode group = response2.SelectSingleNode("group");
+                    XmlNode id = group.SelectSingleNode("id");
                     System.Threading.Thread.Sleep(400);
                     XmlDocument doc1 = new XmlDocument();
-                    doc1.Load("https://api.vk.com/method/groups.invite.xml?user_id=" + a.InnerText + "&group_id=" + textBox2.Text + "&access_token=" + Access_token + "&v=5.73");
+                    doc1.Load("https://api.vk.com/method/groups.invite.xml?user_id=" + a.InnerText + "&group_id=" + id.InnerText + "&access_token=" + Access_token + "&v=5.73");
                     XmlNode response1 = doc1.SelectSingleNode("response");
                     XmlNode user = response.SelectSingleNode("user");
 
